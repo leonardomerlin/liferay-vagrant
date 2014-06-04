e29239226463f95a6f6044c8e7bc4f5f483cb24e
Leonardo Merlin <leonardomerlin@gmail.com>
2014-06-04 15:20:14 -0300

    Fix std stream with tee command

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,7 +4,7 @@
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.old
 
 echo "Set sources to BR"
-sudo sed 's/\/us/\/br/g' /etc/apt/sources.list.old > /etc/apt/sources.list
+sed 's/\/us/\/br/g' /etc/apt/sources.list.old | sudo tee /etc/apt/sources.list
 
 echo "Update the system!"
 sudo apt-get update -y
